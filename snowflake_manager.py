db17fe808593b3b3410140f50803237aed5061e6
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-11-07 15:20:46 +0000

    bugfixes and logging

--- snowflake_manager.py
+++ snowflake_manager.py
@@ -1,32 +1,49 @@
-# %%
-
-# pip install sqlalchemy snowflake-sqlalchemy markdown pyyaml
-
+from logging import getLogger
 import argparse
 import os
 import re
 
 from sqlalchemy import text
+from sqlalchemy.exc import InterfaceError, DatabaseError
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.dialects import registry
 import logging
+from collections import OrderedDict
+
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sql_sections = {
+    "snowflake_setup": "Setting up the dbt User an Roles",
+    "snowflake_import": "Importing Raw Tables",
+    "snowflake_reporter": "Creating Reporter Role"
+}
 
 
 def get_snowflake_connection(account, username, password):
 
     engine = create_engine(
-        f"snowflake://{username}:{password}@{
-            account}/AIRBNB/DEV?warehouse=COMPUTE_WH&role=ACCOUNTADMIN&account_identifier={account}"
+        f"snowflake://{username}:{password}@{account}/AIRBNB/DEV?warehouse=COMPUTE_WH&role=ACCOUNTADMIN&account_identifier={account}"
     )
     connection = engine.connect()
 
     return connection
 
 
+def streamlit_session_id():
+    from streamlit.runtime import get_instance
+    from streamlit.runtime.scriptrunner import get_script_run_ctx
+    runtime = get_instance()
+    session_id = get_script_run_ctx().session_id
+    session_info = runtime._session_mgr.get_session_info(session_id)
+    if session_info is None:
+        raise None
+    return session_info.session.id
+
+
 def get_sql_commands(md):
+    commands = OrderedDict()
+    current_section = None
     in_named_sql = False
     commands_only = ""
     for l in md.split("\n"):
@@ -36,16 +53,22 @@ def get_sql_commands(md):
             else:
                 if l.strip() == "" or l.startswith("--"):
                     continue
-                commands_only += l + "\n"
+                # add command to current section
+                if current_section not in commands:
+                    commands[current_section] = ""
+                commands[current_section] += l + "\n"
         elif l.startswith("```sql {#"):
             in_named_sql = True
-    return [c for c in commands_only.split(";") if c.strip() != ""]
+            current_section = l.split("{#")[1].split("}")[0]
+    return {k: [c.strip("\n") for c in v.split(";") if c.strip() != ""] for k, v in commands.items()}
 
 
 hello_msg = """
-# dbt Zero to Hero Snowflake Importer
+# dbt Zero to Hero Snowflake Importer (beta)
 
-Hi, this webapp helps you set up your Snowflake account
+Hi,
+
+this webapp helps you set up your Snowflake account
 and import the course resources, such as raw tables and user roles
 into your Snowflake account. Simply add the snowflake hostname, username and password
 and then click start setup.
@@ -62,8 +85,20 @@ I'd be very happy if you could provide feedback on wether the tool works and how
 """
 
 
+logging.root.setLevel(logging.INFO)
+logger = getLogger(__name__)
+logger.Formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
 def main():
-    print("Starting Streamlit app")
+    session_id = streamlit_session_id()
+    logger.info("Starting Streamlit app")
+    with open(CURRENT_DIR + "/course-resources.md", "r") as file:
+        md = file.read().rstrip()
+        sql_commands = get_sql_commands(md)
+        print(sql_commands)
+
     registry.register("snowflake", "snowflake.sqlalchemy", "dialect")
     pw = (
         os.environ.get("SNOWFLAKE_PASSWORD")
@@ -72,32 +107,62 @@ def main():
     )
     st.markdown(hello_msg)
     hostname = st.text_input(
-        "Snowflake account name (this looks like as `frgcsyo-ie17820` or `frgcsyo-ie17820.aws`, check your snowlake registration email). _This is not your username, but the first part of the snowflake url you received in your snowflake registration email_:", "")
+        "Snowflake account (this looks like as `frgcsyo-ie17820` or `frgcsyo-ie17820.aws`, check your snowlake registration email).\n\n_**This is not your Snowflake username**, but the first part of the snowflake url you received in your snowflake registration email_:",
+        "jdehewj-vmb00970")
     username = st.text_input(
         "Snowflake username (change this is you didn't set it to `admin` at registration):", "admin")
-    password = st.text_input("Snowflake Password (it will be visible!):", pw)
+    password = st.text_input(
+        "Snowflake Password (it will be visible!):", pw, type="password")
+
     if st.button("Start Setup"):
-
-        with open(CURRENT_DIR + "/course-resources.md", "r") as file:
-            md = file.read().rstrip()
-
+        if len(password) == 0:
+            st.error("Please provide a password")
+            return
         try:
             with st.status("Connecting to Snowflake"):
                 connection = get_snowflake_connection(
                     hostname, username, password)
+        except InterfaceError as e:
+            st.error(
+                f"""Error connecting to Snowflake. This usually means that the snowflake account is invalid.
+                Please verify the snowflake account and try again.\n\nOriginal Error: \n\n{e.orig}""")
+            logging.warning(
+                f"{session_id}: Error connecting to Snowflake. Account: {hostname}, Username: {username}: {e}")
+            return
+        except DatabaseError as e:
+            print(e)
+            st.error(
+                f"Error connecting to Snowflake. This usually means that the snowflake username or password you provided is not valid. Please correct them and retry by pressing the Start Setup button.\n\nOriginal Error:\n\n{e.orig}")
+            logging.warning(
+                f"{session_id}: Error connecting to Snowflake. Account name: {hostname}\n Original Error: {e}")
+            return
         except Exception as e:
             st.error(
-                f"Error connecting to Snowflake. This usually means that the snowflake account name, username or password you provided is not valid. Please correct the account name and retry by pressing the Start Setup button.\n\nOriginal Error:\n\n{e}")
-            logging.warning(f"Error connecting to Snowflake. Account name: {
-                            hostname}\n Original Error: {e}")
+                f"Error connecting to Snowflake.\n\nOriginal Error:\n\n{e.orig}")
+            logging.warning(
+                f"{session_id}: Error connecting to Snowflake. Account name: {hostname}\n Original Error: {e}")
             return
 
-        with st.status("Setting up your Snowflake environment"):
-            sql_commands = get_sql_commands(md)
-            for command in sql_commands:
-                st.write(f"Executing command: `{command}`")
-                connection.execute(text(command))
-        st.toast("Setup complete! You can now go back to the course!", icon="🔥")
+        st.success("Connected to Snowflake successfully!")
+        try:
+            for section, commands in sql_commands.items():
+                with st.status(sql_sections[section]):
+                    for command in commands:
+                        if command.startswith("GRANT USAGE ON SCHEMA AIRBNB.DEV TO ROLE REPORTER"):
+                            command = "GRANT USAGE ON FUTURE SCHEMAS IN DATABASE AIRBNB TO ROLE REPORTER;"
+                            st.write(f"Patching Reporter command: `{command}`")
+                        else:
+                            st.write(f"Executing command: `{command}`")
+                        connection.execute(text(command))
+            st.toast("Setup complete! You can now go back to the course!", icon="🔥")
+            st.success(
+                "Setup complete! You can now go back to the course!", icon="🔥")
+        except Exception as e:
+            st.error(
+                f"Error executing command {command}.\n\nOriginal Error:\n\n{e.orig}")
+            logging.warning(
+                f"{session_id}: Error executing command {command}. Account name: {hostname}\n Original Error: {e}")
+            return
 
 
 if __name__ == "__main__":
--- snowflake_manager.sh
+++ snowflake_manager.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python -m streamlit run snowflake_manager.py --server.runOnSave true  --server.enableCORS false --server.enableXsrfProtection false --server.address 0.0.0.0
+python -m streamlit run snowflake_manager.py --server.runOnSave true  --server.enableCORS false --server.enableXsrfProtection false
