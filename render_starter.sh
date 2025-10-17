90baafe251083fb1dcdabe5d0ad506c634257d24
Zoltan C. Toth <zoltan@zoltanctoth.com>
2025-10-17 16:27:05 +0200

    adding render command

---
+++ render_starter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python -m streamlit run streamlit_app.py \
+  --server.port 10000 --server.address 0.0.0.0
--- snowflake_manager.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python -m streamlit run snowflake_manager.py --server.runOnSave true  --server.enableCORS false --server.enableXsrfProtection false
